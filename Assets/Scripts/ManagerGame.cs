02393b18d84c7b9d6e6f9861727c8916391cda37
Dmitriy <ddnsk54@gmail.com>
2024-12-03 22:45:56 +0300

    Добавил счетчик, таймер . Добавил  сообщения ИГРА ЗАКОНЧЕНА , ВЫ ВЫйграли

--- Assets/Scripts/ManagerGame.cs
+++ Assets/Scripts/ManagerGame.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ManagerGame : MonoBehaviour
@@ -5,14 +6,29 @@ public class ManagerGame : MonoBehaviour
     public static ManagerGame Instance { get; private set; }
 
     public GameObject StartScreen;
+    public GameObject WinScreen;
     public GameObject EndScreen;
     public Ball Ball;
+    public TextMeshProUGUI ScoreBoard;
+    public TextMeshProUGUI Timer;
 
     private bool _isGameRunning = false;
+    private bool _isGameOver = false;
     [SerializeField] private int _score = 0;
+    private float _timeRunning = 0;
+    private int _brickCount = 0;
 
+    private void GameWin()
+    {
+        _isGameRunning = false ;
+        _isGameOver = true ;
+        Destroy(Ball.gameObject);
+        WinScreen.SetActive(true);
+    }
     public void GameOver()
     {
+        _isGameRunning = false;
+        _isGameOver = true;
         Destroy(Ball.gameObject);
         EndScreen.SetActive(true);
     }
@@ -20,39 +36,62 @@ public class ManagerGame : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+        _brickCount = Object.FindObjectsOfType<Brick>().Length;
+        Debug.Log("Количество Brick на сцене: " + _brickCount);
     }
 
     private void Update()
     {
-        if (! _isGameRunning && Input.GetButton("Jump"))
+        if (!_isGameOver)
         {
-            if (Ball != null)
-            {
-                Ball.Accelerate();
-            }
-            else 
-            {
-                Debug.LogError("Ball is not assigned in the inspector.");
-            }
-            
-            _isGameRunning = true;
 
-            if (StartScreen != null)
+
+
+            if (!_isGameRunning && Input.GetButton("Jump"))
             {
-                StartScreen.SetActive(false);
-            }
-            else
-            {
-                Debug.LogError("StartScreen is not assigned in the inspector.");
+                if (Ball != null)
+                {
+                    Ball.Accelerate();
+                }
+                else
+                {
+                    Debug.LogError("Ball is not assigned in the inspector.");
+                }
+
+                _isGameRunning = true;
+
+                if (StartScreen != null)
+                {
+                    StartScreen.SetActive(false);
+                }
+                else
+                {
+                    Debug.LogError("StartScreen is not assigned in the inspector.");
+                }
+
             }
 
+
+            if (_isGameRunning)
+            {
+                _timeRunning += Time.deltaTime;
+
+                Timer.text = ((int)(_timeRunning / 60)) + ":" + ((int)(_timeRunning % 60));
+
+            }
+
+            if (_score >= _brickCount)
+            {
+                GameWin();
+            }
         }
 
-        
+       
     }
 
     public void BrickDestroyed()
     {
         _score++;
+        ScoreBoard.text = _score.ToString();
     }
 }
