982da2c8a64ba877ca0b9ec91922867ebfab1a25
Dmitriy <ddnsk54@gmail.com>
2024-12-08 18:18:56 +0300

    Добавил звуки

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
 
-
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float _speed;
-
     [SerializeField] private int _maxLives = 3; 
-    private int _currentLives; 
-    private Rigidbody _rigidbody;
     [SerializeField] private Transform paddle;
     [SerializeField] private float _respawnHeight = 1.0f;
+    [SerializeField] private AudioClip _hitSoundBall;
+    [SerializeField] private AudioClip _hitSoundBlock;
 
+    private AudioSource _audioSource;
+    private int _currentLives;
+    private Rigidbody _rigidbody;
+    
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _currentLives = _maxLives;
     }
 
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            _audioSource.PlayOneShot(_hitSoundBall);
+        }
+        else if (collision.gameObject.CompareTag("Block"))
+        {
+            _audioSource.PlayOneShot(_hitSoundBlock);
+        }
+    }
 
     public void Accelerate()
     {
